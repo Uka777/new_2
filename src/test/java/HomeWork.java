6689ffdfc76889f0c20cbca7389d99b5bc1d8667
Uka777 <pokatilova.1998@mail.ru>
2022-09-17 17:39:48 +0300

    new tests

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -2,13 +2,15 @@ package guru.qa;
         import org.junit.jupiter.api.Assertions;
         import org.junit.jupiter.api.Test;
 
+        import java.util.Random;
+
 public class HomeWork {
     @Test
     void HomeWork0(){
-        Assertions.assertTrue(4>1);
+        Assertions.assertTrue(new Random().nextInt()>1);
     }
     @Test
     void HomeWork1(){
-        Assertions.assertTrue(2>1);
+        Assertions.assertTrue(new Random().nextInt()>2);
     }
 }
